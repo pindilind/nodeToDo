ac0f0c1f54f416b7b90782a41f4f064e01271300
pindilind <linda.gustafsson@outlook.com>
2021-09-03 11:06:07 +0200

    cdd

--- index.js
+++ index.js
@@ -21,21 +21,58 @@ server.post('/api', (req, res) => {
     let id = 0
     let length = todoItems.length
 
-    if(length > 0) {
+    if (length > 0) {
       id = todoItems[length - 1].id + 1
     }
-    
-    let newTodoItem = {id: id, todoItem: req.body.todoItem}
+
+    let newTodoItem = { id: id, todoItem: req.body.todoItem }
     todoItems.push(newTodoItem)
     fs.writeFileSync("todos.json", JSON.stringify(todoItems))
     res.json(true)
 
 
-  } catch(err) {
+  } catch (err) {
     console.error(err)
     res.status(500).json(false)
   }
 })
+
+server.put('/api/:id', (req, res) => {
+  let id = req.params.id
+  console.log(id)
+
+  let raw = fs.readFileSync("todos.json") //hämtar url till jsonfil
+  let todoItems = JSON.parse(raw)
+
+  let idfound = false
+
+  for (let i = 0; i < todoItems.length; i++) { //plockar ut posision 
+    const element = todoItems[i];
+
+    if (id === element.id.toString()) {
+      element.todoItem = req.body.newText
+      idfound = true
+    } 
+
+  }
+
+
+  if (idfound) {
+    try {
+
+      fs.writeFileSync("todos.json", JSON.stringify(todoItems))
+      res.json(true)
+
+
+    } catch (err) {
+      console.error(err)
+      res.status(500).json(false)
+    }
+  } else {
+    res.json("could nod found todoitem")
+  }
+})
+
 
 server.delete('/api', (req, res) => {
   let id = req.body
@@ -43,22 +80,33 @@ server.delete('/api', (req, res) => {
   let raw = fs.readFileSync("todos.json") //hämtar url till jsonfil
   let todoItems = JSON.parse(raw)
 
-  if(id.id == null) {
+  if (id.id == null) {
+    todoItems = []
     console.log("listan ska rensas")
-  }else {
+  } else {
+
+
+    for (let i = 0; i < todoItems.length; i++) { //plockar ut posision 
+      const element = todoItems[i];
+
+      if (id.id === element.id) {
+        todoItems.splice(i, 1)
+      }
+
+    }
     console.log("item med ID ska tas bort")
   }
 
   try {
-  fs.writeFileSync("todos.json", JSON.stringify(todoItems))
-  res.json(true)
+    fs.writeFileSync("todos.json", JSON.stringify(todoItems))
+    res.json(true)
 
 
-  } catch(err) {
+  } catch (err) {
     console.error(err)
     res.status(500).json(false)
   }
-  
+
 })
 
 server.use(express.static('public'))
--- public/public.js
+++ public/public.js
@@ -30,7 +30,7 @@ getToDoText.innerText = 'Hämtar ToDo' //skriver text på knapp
 bodyDiv.appendChild(getToDoBtn)
 getToDoBtn.appendChild(getToDoText)
 
-async function getTodo() { //onclick funktion 
+/* async function getTodo() { //onclick funktion 
     const displayToDo = await makeRequest("http://localhost:3000/api", "GET")
     console.log(displayToDo)
     
@@ -49,9 +49,8 @@ async function getTodo() { //onclick funktion
 
         document.getElementById('toDoDiv').appendChild(element)
         /* toDoText.innerText = element. */
-    }
-}
-
+/*   }
+}  */
 getToDoBtn.onclick = getTodo
 
 
@@ -64,19 +63,106 @@ deleteBtn.appendChild(btnText)
 
 
 async function deleteTodo(id) { //onclick funktion 
-  
-    if(id != null) {
+
+    if (id != null) {
         console.log(id)
     }
 
-    const status = await makeRequest("http://localhost:3000/api", "DELETE", {id})
+    const status = await makeRequest("http://localhost:3000/api", "DELETE", { id })
     console.log(status)
 
     getTodo()
 }
 
-deleteBtn.onclick = deleteTodo
+deleteBtn.onclick = function () {
+    deleteTodo()
+}
 
+
+let putBtn = document.createElement("button")
+let putText = document.createElement("h3")
+putText.innerText = "Ändra"
+
+bodyDiv.appendChild(putBtn)
+putBtn.appendChild(putText)
+
+putBtn.onclick = changeItem
+
+function changeItem(id) {
+
+
+}
+
+
+
+async function getTodo(id) { //onclick funktion 
+
+    console.log('id in getTodo: ' + JSON.stringify(id))
+
+    // hämta listan eller enstaka todoItem
+    const displayToDo = await makeRequest("http://localhost:3000/api", "GET")
+    console.log(displayToDo)
+
+    const todoDiv = document.getElementById('toDoDiv')
+
+    //töm diven
+    todoDiv.innerHTML = ""
+
+    for (let i = 0; i < displayToDo.length; i++) {
+        console.log(i)
+        const id = displayToDo[i].id
+        const listItem = document.createElement('li')
+        const textElement = document.createElement('input')
+        textElement.value = displayToDo[i].todoItem
+        textElement.disabled = true
+        const changeTodoItem = document.createElement('button')
+        changeTodoItem.innerText = 'Ändra'
+        changeTodoItem.onclick = () => {
+            console.log('Ändra todoItem med id ' + id)
+            textElement.disabled = false
+            changeTodoItem.innerText = 'Spara ändring'
+            changeTodoItem.onclick = () => {
+                /* updateTodo(id, textElement.value) */
+
+                updateTodo(id, textElement.value)
+            }
+        }
+        const deleteTodoItem = document.createElement('button')
+        deleteTodoItem.innerText = 'Ta bort'
+        deleteTodoItem.onclick = function () {
+            let id = displayToDo[i].id
+            deleteTodo(id)
+        }
+
+        listItem.appendChild(textElement)
+        listItem.appendChild(changeTodoItem)
+        listItem.appendChild(deleteTodoItem)
+
+
+        todoDiv.appendChild(listItem)
+        /* toDoText.innerText = element. */
+    }
+}
+
+async function updateTodo(id, newText) {
+
+    const result = await makeRequest("http://localhost:3000/api/" + id, "PUT", {newText: newText})
+    console.log(result)
+    
+}
+
+let getOneItemBtn = document.createElement("button")
+let getOneText = document.createElement("h3")
+getOneText.innerText = "Hämta 1"
+
+bodyDiv.appendChild(getOneItemBtn)
+getOneItemBtn.appendChild(getOneText)
+
+getOneItemBtn.onclick = getOneItem
+
+function getOneItem() {
+
+}
 
 async function makeRequest(url, method, body) {
     try {
